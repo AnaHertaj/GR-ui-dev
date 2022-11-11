71c3151646398b289e8db3e35ce215704838c074
M.O.D.E.R.N.US <anushertaj@gmail.com>
2022-11-11 13:38:28 +0100

    upload content

---
+++ js/functions.js
@@ -0,0 +1,12 @@
+
+$('.slider').slick({
+    dots: true,
+    responsive: [
+        {
+          breakpoint: 1024,
+          settings: {
+            arrows: false
+          }
+        }
+    ]
+});
